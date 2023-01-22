909b12e8797f960a1ba4e89cb55fad3c5bf45b6d
a.lokhvitskiy <a.lokhvitskiy@gagaringroup.com>
2023-01-22 02:09:56 +0000

    esbuild & component example

--- src/core/component.ts
+++ src/core/component.ts
@@ -1,14 +1,6 @@
-export class PGComponent extends HTMLElement {
-
-	private readonly _tag: string
-
-	constructor(tag23: string) {
-		super()
-		console.log(PGComponent.name);
-		if (!tag23) {
-			console.error('Invalid tag!')
-		}
-		this._tag = tag23
-	}
-
+export interface PGComponent {
+	connectedCallback?: () => void
+	disconnectedCallback?: () => void
+	attributeChangedCallback?: () => void
+	observedAttributes?: string[]
 }
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,15 @@
 import {PGComponent} from './core/component';
 
-class Test extends HTMLElement {
-
-	static observedAttributes = ['message'];
-	private _name: string | null | undefined;
+class PGTestComponent extends HTMLElement implements PGComponent {
 
 	constructor() {
 		super();
-		this.addEventListener('change', this.callMe.bind(this))
 	}
 
 	connectedCallback() {
-		console.log('created!')
-		console.log(this.getAttribute('message'))
-	}
-
-	attributeChangedCallback(name: string, oldValue: any, newValue: any) {
-		console.log('ATTR CHANGED:', name, oldValue, newValue)
-		this._name = this.getAttribute('message');
-	}
-
-	public callMe() {
-		console.log('changed')
+		console.log('OMG IT WORKS???')
 	}
 
 }
 
-customElements.define('test-el', Test)
-
-setTimeout(() => {
-	const el: any = document.querySelector('test-el')
-	if (el) {
-		el.innerHTML = 'Content changed!'
-	}
-}, 1000)
-
-class PGTestComponent extends PGComponent {
-	constructor(props: any) {
-		super(props);
-	}
-
-}
-
-const pgt = new PGTestComponent('testy')
+customElements.define('pg-el', PGTestComponent)
